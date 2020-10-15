75350786326f0d812ac1c697065c5ea39bbd4b17
leticek <smiead@seznam.cz>
2020-10-15 16:30:56 +0200

    .

--- y/script.js
+++ y/script.js
@@ -1,9 +1,62 @@
 window.addEventListener('load', (event) => {
+    setTimer();
     let currTime = new Date();
     document.getElementById("currentTime").innerHTML = currTime.toLocaleDateString("cs-CZ") + " " + currTime.toLocaleTimeString("cs-CZ");
     showTime();
 });
 
+function setTimer() {
+    var minTime = 26100;
+    var maxTime = 69300;
+    var currDate = new Date();
+    currTime = currDate.getSeconds() + currDate.getMinutes() * 60 + currDate.getHours() * 3600;
+    if (currDate.getDay() <= 5) {
+        if (currTime >= minTime && currTime <= maxTime) {
+            let row = document.getElementById(currDate.getDay());
+            console.log(row.id);
+            let marker = document.getElementById('timeIndicator');
+            switch (currDate.getDay()) {
+                case 1:
+                    var timeFraction = (currTime - minTime) / (maxTime - minTime);
+                    var width = row.getBoundingClientRect()['width'] - document.getElementById('th_column').getBoundingClientRect()['width'];
+                    var position = width * timeFraction;
+                    marker.style.top = row.getBoundingClientRect()['top'] + 'px';
+                    marker.style.left = position + 'px';
+                    break;
+                case 2:
+                    var timeFraction = (currTime - minTime) / (maxTime - minTime);
+                    var width = row.getBoundingClientRect()['width'] - document.getElementById('th_column').getBoundingClientRect()['width'];
+                    var position = width * timeFraction;
+                    marker.style.top = row.getBoundingClientRect()['top'] + 'px';
+                    marker.style.left = position + 'px';
+                    break;
+                case 3:
+                    var timeFraction = (currTime - minTime) / (maxTime - minTime);
+                    var width = row.getBoundingClientRect()['width'] - document.getElementById('th_column').getBoundingClientRect()['width'];
+                    var position = width * timeFraction;
+                    marker.style.top = row.getBoundingClientRect()['top'] + 'px';
+                    marker.style.left = position + 'px';
+                    break;
+                case 4:
+                    var timeFraction = (currTime - minTime) / (maxTime - minTime);
+                    var width = row.getBoundingClientRect()['width'] - document.getElementById('th_column').getBoundingClientRect()['width'];
+                    var position = width * timeFraction;
+                    marker.style.top = row.getBoundingClientRect()['top'] + 'px';
+                    marker.style.left = position + 'px';
+                    break;
+                case 5:
+                    var timeFraction = (currTime - minTime) / (maxTime - minTime);
+                    var width = row.getBoundingClientRect()['width'] - document.getElementById('th_column').getBoundingClientRect()['width'];
+                    var position = width * timeFraction;
+                    marker.style.top = row.getBoundingClientRect()['top'] + 'px';
+                    marker.style.left = position + 'px';
+                    break;
+                default:
+                    console.log('def');
+            }
+        }
+    }
+}
 
 function showTime() {
     setInterval(temp, 1000);
@@ -13,6 +66,7 @@ function temp() {
     let currTime = new Date();
     let time = currTime.toLocaleDateString("cs-CZ") + " " + currTime.toLocaleTimeString("cs-CZ");
     document.getElementById("currentTime").innerHTML = time;
+    setTimer();
 }
 
 
