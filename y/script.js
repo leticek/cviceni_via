cc23cf20cea9adad92259dba21553f8a5dc907cd
leticek <smiead@seznam.cz>
2020-10-15 17:24:15 +0200

    .

--- y/script.js
+++ y/script.js
@@ -1,4 +1,9 @@
-window.addEventListener("load", showTime());
+window.addEventListener('load', (event) => {
+    let currTime = new Date();
+    document.getElementById("currentTime").innerHTML = currTime.toLocaleDateString("cs-CZ") + " " + currTime.toLocaleTimeString("cs-CZ");
+    showTime();
+});
+
 
 function showTime() {
     setInterval(temp, 1000);
@@ -6,30 +11,17 @@ function showTime() {
 
 function temp() {
     let currTime = new Date();
-
     let time = currTime.toLocaleDateString("cs-CZ") + " " + currTime.toLocaleTimeString("cs-CZ");
-    document.getElementById("currentTime").innerText = time;
+    document.getElementById("currentTime").innerHTML = time;
 }
 
-function showLecture() {
-    let cells = document.getElementsByClassName("lecture");
+
+function highlightClass(classToHighlight, classToHide) {
+    let cells = document.getElementsByClassName(classToHighlight);
     for (i = 0; i < cells.length; i++) {
         cells[i].style.opacity = 1;
     }
-
-    cells = document.getElementsByClassName("seminar");
-    for (i = 0; i < cells.length; i++) {
-        cells[i].style.opacity = 0.4;
-    }
-}
-
-function showSeminar() {
-    let cells = document.getElementsByClassName("seminar");
-    for (i = 0; i < cells.length; i++) {
-        cells[i].style.opacity = 1;
-    }
-
-    cells = document.getElementsByClassName("lecture");
+    cells = document.getElementsByClassName(classToHide);
     for (i = 0; i < cells.length; i++) {
         cells[i].style.opacity = 0.4;
     }
