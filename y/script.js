97472c4ba98735304bd9e6b0168c70ecff87d0bd
leticek <smiead@seznam.cz>
2020-11-04 20:28:11 +0100

    notes added

--- y/script.js
+++ y/script.js
@@ -1,9 +1,77 @@
+var classSource = "";
+
 window.addEventListener('load', (event) => {
-    setTimer();
+    //setTimer();
     let currTime = new Date();
     document.getElementById("currentTime").innerHTML = currTime.toLocaleDateString("cs-CZ") + " " + currTime.toLocaleTimeString("cs-CZ");
     showTime();
 });
+
+function closeDialog() {
+    document.getElementById("noteDialog").open = false;
+    var notesHTML = document.getElementById("notes");
+    while (notesHTML.hasChildNodes()) {
+        notesHTML.removeChild(notesHTML.lastChild);
+    }
+}
+
+function noteClicked(id) {
+    var isOpen = document.getElementById("noteDialog").open;
+    if (isOpen) {
+        closeDialog();
+    } else {
+        document.getElementById("noteDialog").open = true;
+        classSource = id;
+        var notesHTML = document.getElementById("notes");
+        for (var i = 0; i < localStorage.length; i++) {
+            var item = JSON.parse(localStorage.getItem(localStorage.key(i)));
+            if (item["source"] == classSource) {
+                notesHTML.append(localStorage.key(i) + ": " + item["value"]);
+                notesHTML.appendChild(document.createElement('br'));
+            }
+        }
+    }
+}
+
+function saveNote() {
+    var note = document.getElementById("noteText").value;
+    var time = new Date();
+    var json = JSON.stringify({ "source": classSource.toString(), "value": note.toString() });
+    localStorage.setItem(time.toLocaleDateString("cs-CZ") + " " + time.toLocaleTimeString("cs-CZ"), json);
+    document.getElementById("noteText").value = "";
+    closeDialog();
+}
+
+function showTime() {
+    setInterval(() => {
+        let currTime = new Date();
+        let time = currTime.toLocaleDateString("cs-CZ") + " " + currTime.toLocaleTimeString("cs-CZ");
+        document.getElementById("currentTime").innerHTML = time;
+        //setTimer();
+    }, 1000);
+}
+
+function highlightClass(classToHighlight, classToHide) {
+    let cells = document.getElementsByClassName(classToHighlight);
+    for (i = 0; i < cells.length; i++) {
+        cells[i].style.opacity = 1;
+    }
+    cells = document.getElementsByClassName(classToHide);
+    for (i = 0; i < cells.length; i++) {
+        cells[i].style.opacity = 0.4;
+    }
+}
+
+function showAll() {
+    let cells = document.getElementsByClassName("seminar");
+    for (i = 0; i < cells.length; i++) {
+        cells[i].style.opacity = 1;
+    }
+    cells = document.getElementsByClassName("lecture");
+    for (i = 0; i < cells.length; i++) {
+        cells[i].style.opacity = 1;
+    }
+}
 
 function setTimer() {
     var minTime = 26100;
@@ -55,39 +123,5 @@ function setTimer() {
                     console.log('def');
             }
         }
-    }
-}
-
-function showTime() {
-    setInterval(temp, 1000);
-}
-
-function temp() {
-    let currTime = new Date();
-    let time = currTime.toLocaleDateString("cs-CZ") + " " + currTime.toLocaleTimeString("cs-CZ");
-    document.getElementById("currentTime").innerHTML = time;
-    setTimer();
-}
-
-
-function highlightClass(classToHighlight, classToHide) {
-    let cells = document.getElementsByClassName(classToHighlight);
-    for (i = 0; i < cells.length; i++) {
-        cells[i].style.opacity = 1;
-    }
-    cells = document.getElementsByClassName(classToHide);
-    for (i = 0; i < cells.length; i++) {
-        cells[i].style.opacity = 0.4;
-    }
-}
-
-function showAll() {
-    let cells = document.getElementsByClassName("seminar");
-    for (i = 0; i < cells.length; i++) {
-        cells[i].style.opacity = 1;
-    }
-    cells = document.getElementsByClassName("lecture");
-    for (i = 0; i < cells.length; i++) {
-        cells[i].style.opacity = 1;
     }
 }
